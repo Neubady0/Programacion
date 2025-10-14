3bd7ff75d1fce357b344de927b6324263aac3e9a
Neubady <eric.lopezguerrero16@gmail.com>
2025-10-14 16:07:49 +0200

    Delete matriculas_alumnos directory

--- matriculas_alumnos/dominio/alumno.py
+++
@@ -1,7 +0,0 @@
-class Alumno:
-    """Representa a un objeto alumno."""
-    def __init__(self, nombre: str):
-        self.nombre = nombre.strip()
-
-    def __str__(self) -> str:
-        return f"Alumno: {self.nombre}"
